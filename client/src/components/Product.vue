be7039b18f9210717c48428f03a7fea3536d5d04
Chris Crawford <christopher.crawford1@outlook.com>
2021-07-27 12:31:39 +0100

    Extracted reusable code into its own Vue component

---
+++ client/src/components/Product.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card">
+        <img :src="require('../assets/' + image_name)" :alt="alt" style="width:50%">
+        <h1>{{ name }}</h1>
+        <p class="price">£{{ price }}</p>
+        <p>{{ blurb }}</p>
+        <p>
+            <button @click="updateCart">Add To Cart</button>
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Product',
+  props:['image_name', 'name', 'price', 'blurb', 'alt'],
+  emits: ['addToCart'],
+  data: function () {
+    return {
+
+    }
+  },
+
+
+  methods: {
+      updateCart() {
+          const item = {
+              name: this.name,
+              amount: parseInt(this.price),
+          };
+
+          this.$emit('addToCart', item);
+      }
+  }
+
+}
+</script>
