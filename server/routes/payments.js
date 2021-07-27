8ac8df5c3766d6282e11009c2846224f5f1ab5cb
Chris Crawford <christopher.crawford1@outlook.com>
2021-07-27 11:00:46 +0100

    Send dynamic data retrieved via the request body to Primer

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -5,24 +5,21 @@ const router = express.Router();
 const axios = require('axios');
 
 
-router.get('/create', async (req, res) => {
+router.post('/create', async (req, res) => {
     const paymentUrl = process.env.SANDBOX_API + "/payments";
 
     const config = {
         headers: { "X-API-KEY": process.env.PRIMER_API_KEY}
     };
 
-    // const paymentInstrumentToken = req.body.token;
-
-    const testToken = 'h0a4WNxfQT6UuWwan5_CY3wxNjI3MzczNjkw';
     const payload = {
-        amount: 2000,
-        currencyCode: "GBP",
-        orderId: "Order 2",
+        amount: req.body.amount,
+        currencyCode: req.body.currencyCode,
+        orderId: req.body.orderId,
         paymentInstrument: {
-            token: testToken,
+            token: req.body.token
         },
-        statementDescriptor: "Test Purchase 2"
+        statementDescriptor: req.body.statement
     }
 
     axios.post(paymentUrl, payload, config)
@@ -33,10 +30,6 @@ router.get('/create', async (req, res) => {
         res.send(error);
     })
 });
-
-router.post('/check', async (req, res) => {
-    console.log(req.body);
-})
 
 
 module.exports = router;
